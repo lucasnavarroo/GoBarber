e7dfefa9bdbc579f2d5359f2ff2766f6e05be645
Lucas Navarro <lucasnavarro979@gmail.com>
2019-03-03 17:52:11 -0300

    configuracoes

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: '1234',
+  database: 'gonodemodulo2',
+  operatorAliases: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+}
